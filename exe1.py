b065a0557c746baaf8da6f67e9bf4f4f4973584b
Salazar-GodDark <gabriel.kohut.ribeiro@escola.pr.gov.br>
2025-09-19 10:32:13 -0300

    Add files via upload

--- exe1.py
+++ exe1.py
@@ -1,1 +1,12 @@
-oi
+contagem_pares = 0
+contagem_impares = 0
+
+for _ in range(10):
+    numero = int(input("Digite um número:  "))
+    if numero % 2 == 0:
+        contagem_pares += 1
+    else:
+       contagem_impares += 1
+
+print(f"Total pares: {contagem_pares}")
+print(f"Total impares: {contagem_impares}")
